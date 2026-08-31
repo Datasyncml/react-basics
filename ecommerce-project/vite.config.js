e54e3e5eec73ca07d3d8963cac77e9da8f16cde6
Md Shyaan <shyaanmd707@gmail.com>
2026-09-01 00:27:50 +0530

    added server proxy config for/api and /images

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'starting-code',
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000'
+      },
+      '/images':{
+        target:'http://localhost:3000'
+      }
+    }
+  }
 })
